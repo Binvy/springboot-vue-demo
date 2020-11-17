62876e0067f3aefb1d599d074045306c4e52f8b8
hanbinwei <hbw4425625@163.com>
2020-11-17 22:12:14 +0800

    jvm3 chapter8

---
+++ src/main/java/com/binvi/springboot/demo03/book/jvm/chapter8/CalcTest.java
@@ -0,0 +1,18 @@
+package com.binvi.springboot.demo03.book.jvm.chapter8;
+
+/**
+ * @author binvi
+ * @version 1.0
+ * @Description: 基于栈的解释器执行过程
+ * @date 2020/11/17 22:06
+ */
+public class CalcTest {
+
+	public int calc() {
+		int a = 100;
+		int b = 200;
+		int c = 300;
+		return (a + b) * c;
+	}
+
+}
--- src/main/java/com/binvi/springboot/demo03/book/jvm/chapter8/Test.java
+++ src/main/java/com/binvi/springboot/demo03/book/jvm/chapter8/Test.java
@@ -1,14 +1,14 @@
 package com.binvi.springboot.demo03.book.jvm.chapter8;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-
-import static java.lang.invoke.MethodHandles.lookup;
+import java.lang.reflect.Field;
 
 /**
  * @author binvi
  * @version 1.0
- * @Description:
+ * @Description: 子类调用祖父类方法
  * @date 2019/9/19 22:50
  */
 public class Test {
@@ -28,9 +28,18 @@ public class Test {
 	class Son extends Father {
 		void thinking() {
 			try {
+				// JDK 7 Update 9 之前如下代码可正常运行，并输出"i am grandfather"，该版本之后，会输出"i am father"
+				// MethodType methodType = MethodType.methodType(void.class);
+				// MethodHandle methodHandle = lookup().findSpecial(GrandFather.class, "thinking", methodType, getClass());
+				// methodHandle.invoke(this);
+
+				// 通用版本
 				MethodType methodType = MethodType.methodType(void.class);
-				MethodHandle methodHandle = lookup().findSpecial(GrandFather.class, "thinking", methodType, getClass());
-				methodHandle.invoke(this);
+				Field lookupImpl = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+				lookupImpl.setAccessible(true);
+				MethodHandle mh = ((MethodHandles.Lookup) lookupImpl.get(null))
+						.findSpecial(GrandFather.class, "thinking", methodType, GrandFather.class);
+				mh.invoke(this);
 			} catch (Throwable e) {
 
 			}
